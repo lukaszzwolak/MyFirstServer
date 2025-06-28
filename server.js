2561ef992a58126565e8f3f8fb14431440101e38
lukaszzwolak <lukasz.zwolak1607@gmail.com>
2025-06-28 17:48:10 +0200

    Refaktor do Handlebars

--- server.js
+++ server.js
@@ -1,41 +1,57 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+// Konfiguracja silnika HBS
+app.engine("hbs", hbs());
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "/views"));
 
+// Pliki statyczne
 app.use(express.static(path.join(__dirname, "/public")));
 
+// Middleware blokujący dostęp do /user/*
+app.use("/user", (req, res) => {
+  res.render("forbidden", { layout: false });
+});
+
+// Endpointy
 app.get("/", (req, res) => {
-  res.show("index.html");
+  res.render("index", { layout: false });
+});
+
+app.get("/home", (req, res) => {
+  res.render("index", { layout: false });
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { layout: false, name: req.params.name });
 });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about", { layout: false });
 });
 
 app.get("/contact", (req, res) => {
-  res.show("contact.html");
+  res.render("contact", { layout: false });
 });
 
 app.get("/info", (req, res) => {
-  res.show("info.html");
+  res.render("info", { layout: false });
 });
 
 app.get("/history", (req, res) => {
-  res.show("history.html");
+  res.render("history", { layout: false });
 });
 
+// Obsługa 404
 app.use((req, res) => {
-  res.status(404).send("404 not found...");
+  res.status(404).render("404", { layout: false });
 });
 
+// Start serwera
 app.listen(8000, () => {
   console.log("Server is running on port: 8000");
 });
